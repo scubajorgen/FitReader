3f9b7f07d50471296ee5a1568e13ca0408a23f34
Jorgen <scubajorgen@gmail.com>
2017-07-07 22:24:54 +0200

    Extended the FitReaderTest

--- src/test/java/net/studioblueplanet/fitreader/FitReaderTest.java
+++ src/test/java/net/studioblueplanet/fitreader/FitReaderTest.java
@@ -12,6 +12,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+
 import net.studioblueplanet.logger.DebugLogger;
 
 /**
@@ -60,72 +65,115 @@ public class FitReaderTest
     }
 
     
-    private void waypointTest()
+    /**
+     * Reads a test FIT file.
+     * @return 
+     */
+    private FitRecordRepository readTestFitFile()
+    {
+        FitRecordRepository repository;     
+        FitReader           reader;
+        InputStream         in;
+        byte[]              inputBytes;        
+      
+        reader=FitReader.getInstance();
+        
+        // Simple fit file: file_id (1 record), file_created (1 record), waypoints (2 records). All one record
+        inputBytes=javax.xml.bind.DatatypeConverter.parseHexBinary("0E107D06180100002E4649540000"+                          // header
+                
+                                                                   "40000000000603048C040486010284020284050284000100"+      // definition message 0x00 - file_id
+                                                                   "0082224BEAFFFFFFFF01001F06FFFF08"+                      // data message 0x01
+                                                                   
+                                                                   "410000310002000284010102"+                              // definition message 0x01 - file_created
+                                                                   "01FE01FF"+
+                
+                                                                   "4200001D0009FD0486001007010485020485FE0284030284040284050284063207"+    // definition message 0x02 - waypoints
+                                                                   "0247FD6432746573745F74726B202D205374617200"+                            // data message 0x02
+                                                                   "04462225A979F00400005F00FFFFFFFF"+
+                                                                   "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"+
+                                                                   "02FFFFFFFF4761726D696E2054616977616E000000"+                            // data message 0x02
+                                                                   "C25BD2114DEF7F5605005E00820AFFFF"+
+                                                                   "7468697320697320736f6d652066756e6e792074657874207468617420736572766573206173206465736372697074696f6E"+
+                
+                                                                   "0000");                                                                 // crc
+        in=new ByteArrayInputStream(inputBytes);
+
+        repository=reader.readInputStream(in);
+
+        return repository;
+    }
+    
+    
+    @Test
+    public void testReadFile()
     {
         FitRecordRepository repository;
         FitRecord           record;
         int                 size;
         int                 i;
-        FitReader           reader;
 
+        System.out.println("readFile");
         DebugLogger.setDebugLevel(DebugLogger.DEBUGLEVEL_INFO);
 
-        FitGlobalProfile.getInstance();
+        repository=this.readTestFitFile();
         
-        reader=FitReader.getInstance();
-        
-        System.out.println(System.getProperty("user.dir"));
-
-        repository=reader.readFile("target/classes/test_waypoints.fit");
-
-        
-        record=repository.getFitRecord("user_profile");
-
-        DebugLogger.info("User name: "+record.getStringValue(0, "friendly_name"));
-        
-        record=repository.getFitRecord("record");
-        size=record.getNumberOfRecordValues();
-        i=0;
-        while (i<size)
-        {
-            DebugLogger.info("Coor "+record.getTimeValue(i, "timestamp").toString()+" "+
-                             record.getLatLonValue(i, "position_lat")+", "+record.getLatLonValue(i, "position_long")+
-                             " alt "+record.getAltitudeValue(i, "altitude")+
-                             " speed "+record.getSpeedValue(i, "speed")+
-                             " distance "+record.getDistanceValue(i, "distance")+
-                             " temp "+record.getIntValue(i, "temperature"));
-            i++;
-        }
-        
-        repository=reader.readFile("target/classes/Locations.fit");
         record=repository.getFitRecord("waypoints");
         size=record.getNumberOfRecordValues();
-        i=0;
-        while (i<size)
-        {
-            DebugLogger.info("Waypoint "+record.getIntValue(i, "message_index")+
-                             ": "+record.getStringValue(i, "name")+
-                             " "+record.getStringValue(i, "description")+
-                             " "+record.getTimeValue(i, "timestamp").toString()+
-                             " "+record.getLatLonValue(i, "position_lat")+", "+record.getLatLonValue(i, "position_long")+
-                             " symbol "+record.getIntValue(i, "symbol")+
-                             " alt "+record.getAltitudeValue(i, "altitude")+
-                             " unknown "+record.getIntValue(i, "unknown")
-                              );
-            i++;
-        }        
+    
+        assertEquals(size                                                     , 2);
+        assertEquals(record.getIntValue         (0, "message_index")          , 0);
+        assertEquals(record.getStringValue      (0, "name")                   ,"test_trk - Star");
+        assertEquals(record.getStringValue      (0, "description")            ,"");
+        assertEquals(record.getTimeValue        (0, "timestamp").toString()   ,"2016-10-15 15:20:39.000000000");
+        assertEquals(record.getLatLonValue      (0, "position_lat")           ,52.21951995044947, 0.00000001);
+        assertEquals(record.getLatLonValue      (0, "position_long")          ,6.945969918742776, 0.00000001);
+        assertEquals(record.getIntValue         (0, "symbol")                 ,95);
+        assertEquals(record.getAltitudeValue    (0, "altitude")               ,12607.0, 0.1);
+        assertEquals(record.getIntValue         (0, "unknown")                ,0xffff);
+        
+        
+        assertEquals(record.getIntValue         (1, "message_index")          , 5);
+        assertEquals(record.getStringValue      (1, "name")                   ,"Garmin Taiwan");
+        assertEquals(record.getStringValue      (1, "description")            ,"this is some funny text that serves as description");
+        assertEquals(record.getTimeValue        (1, "timestamp").toString()   ,"1989-12-30 23:59:59.000000000");
+        assertEquals(record.getLatLonValue      (1, "position_lat")           ,25.061783362179995, 0.00000001);
+        assertEquals(record.getLatLonValue      (1, "position_long")          ,121.64026667363942, 0.00000001);
+        assertEquals(record.getIntValue         (1, "symbol")                 ,94);
+        assertEquals(record.getAltitudeValue    (1, "altitude")               ,38.0, 0.1);
+        assertEquals(record.getIntValue         (1, "unknown")                ,0xffff);
         
     }
     
-    
-    
-    /**
-     * Test of readFile method, of class FitReader.
-     */
     @Test
-    public void testReadFile()
+    public void testRecordRepository()
     {
-        this.waypointTest();
+        FitRecordRepository repository;
+        ArrayList<String>   messages;
+        
+        System.out.println("FitRecordRepository");
+        DebugLogger.setDebugLevel(DebugLogger.DEBUGLEVEL_INFO);
+        
+        repository=this.readTestFitFile();
+        
+        messages=repository.getMessages();
+        
+        assertEquals(messages.get(0), "file_id");
+        assertEquals(messages.get(1), "file_creator");
+        assertEquals(messages.get(2), "waypoints");
+        
+        assertEquals(repository.recordExists(0), true);
+        assertEquals(repository.recordExists(1), true);
+        assertEquals(repository.recordExists(2), true);
+        assertEquals(repository.recordExists(3), false);
+        
+        assertNotEquals(repository.getFitRecord("file_id"), null);
+        assertEquals(repository.getFitRecord("blah_blah"), null);
+        
+        assertNotEquals(repository.getFitRecord(0), null);
+        assertEquals(repository.getFitRecord(4), null);
+        assertEquals(repository.getFitRecord(-1), null);
+        
+     
     }
-    
+
 }
